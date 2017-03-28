06c5c2c956f704a080a75c5b15b8b689fd18694d
magic <zhongguovu@gmail.com>
2017-03-28 17:03:38 +0800

    fix bug

--- app.js
+++ app.js
@@ -8,69 +8,70 @@ App({
 
     var newOpenid = wx.getStorageSync('openid')
     if (!newOpenid) {
-      wx.getUserInfo({
-        success: function (result) {
-
-          var userInfo = result.userInfo;
-          var nickName = userInfo.nickName;
-          var avatarUrl = userInfo.avatarUrl;
 
 
-          wx.login({
-            success: function (res) {
-              user.loginWithWeapp(res.code).then(function (user) {
-                var openid = user.get("authData").weapp.openid;
-                console.log(user, 'user', user.id, res);
+      wx.login({
+        success: function (res) {
+          user.loginWithWeapp(res.code).then(function (user) {
+            var openid = user.get("authData").weapp.openid;
+            console.log(user, 'user', user.id, res);
 
-                if (user.get("nickName")) {
-                  // 第二次访问
-                  console.log(user.get("nickName"), 'res.get("nickName")');
+            if (user.get("nickName")) {
+              // 第二次访问
+              console.log(user.get("nickName"), 'res.get("nickName")');
 
-                  wx.setStorageSync('openid', openid)
-                } else {
+              wx.setStorageSync('openid', openid)
+            } else {
+
+              var u = Bmob.Object.extend("_User");
+              var query = new Bmob.Query(u);
+              query.get(user.id, {
+                success: function (result) {
+                  wx.setStorageSync('own', result.get("uid"));
+                },
+                error: function (result, error) {
+                  console.log("查询失败");
+                }
+              });
+
+
+              //保存用户其他信息
+              wx.getUserInfo({
+                success: function (result) {
+
+                  var userInfo = result.userInfo;
+                  var nickName = userInfo.nickName;
+                  var avatarUrl = userInfo.avatarUrl;
 
                   var u = Bmob.Object.extend("_User");
                   var query = new Bmob.Query(u);
+                  // 这个 id 是要修改条目的 id，你在生成这个存储并成功时可以获取到，请看前面的文档
                   query.get(user.id, {
                     success: function (result) {
-                      wx.setStorageSync('own', result.get("uid"));
-                    },
-                    error: function (result, error) {
-                      console.log("查询失败");
+                      // 自动绑定之前的账号
+
+                      result.set('nickName', nickName);
+                      result.set("userPic", avatarUrl);
+                      result.set("openid", openid);
+                      result.save();
+
                     }
                   });
 
-
                 }
-
-
-
-                var u = Bmob.Object.extend("_User");
-                var query = new Bmob.Query(u);
-                // 这个 id 是要修改条目的 id，你在生成这个存储并成功时可以获取到，请看前面的文档
-                query.get(user.id, {
-                  success: function (result) {
-                    // 自动绑定之前的账号
-
-                    result.set('nickName', nickName);
-                    result.set("userPic", avatarUrl);
-                    result.set("openid", openid);
-                    result.save();
-
-                  }
-                });
-
-                
-              }, function (err) {
-                console.log(err, 'errr');
               });
 
+
             }
+
+          }, function (err) {
+            console.log(err, 'errr');
           });
+
         }
       });
-
     }
+
 
 
   },
